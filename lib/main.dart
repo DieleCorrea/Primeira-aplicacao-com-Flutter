0a2eb4c83b238c9f403574d506aa041512ec4e65
DieleCorrea <dybs1520@gmail.com>
2023-09-11 10:48:20 -0300

    criei so para rascunho

--- lib/main.dart
+++ lib/main.dart
@@ -16,104 +16,26 @@ class MyApp extends StatelessWidget {
               seedColor: Color.fromARGB(255, 224, 217, 221)),
           useMaterial3: true,
         ),
-        home: Container(
-          color: const Color.fromARGB(255, 91, 151, 199),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Stack(
-                alignment: AlignmentDirectional.centerStart,
-                children: [
-                  Container(
-                    color: Colors.pink,
-                    width: 500,
-                    height: 100,
-                  ),
-                  Container(
-                    color: Colors.blue,
-                    width: 50,
-                    height: 50,
-                  ),
-                ],
-              ),
-              Container(
-                color: Colors.white,
-                width: 500,
-                height: 100,
-                child: Text(
-                    'aaaaaaa',
-                    style: ,
-                ),
-              ),
-              Stack(
-                alignment: AlignmentDirectional.centerStart,
-                children: [
-                  Container(
-                    color: Colors.pink,
-                    width: 500,
-                    height: 100,
-                  ),
-                  Container(
-                    color: Colors.blue,
-                    width: 50,
-                    height: 50,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    color: Colors.black,
-                    width: 50,
-                    height: 50,
-                  ),
-                  Container(
-                    color: Colors.pink,
-                    width: 50,
-                    height: 50,
-                  ),
-                  Container(
-                    color: Colors.black,
-                    width: 50,
-                    height: 50,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(color: Colors.black,width: 60,height: 60,),
-                  Container(color: Colors.pink, width: 200, height: 60,),
-                  Container(color: Colors.black, width: 60,height: 60,),
-                ],
-              ),
-              Container(//texto
-                color: Color.fromARGB(255, 219, 150, 174),
-                width: 300,
-                height: 30,
-                //Inserir texto crio um filho widget chamado text
-                child: Text(
-                  'Diamante amarelo',
-                  style: TextStyle(//estilo do texto 
-                  color: Colors.black,//cor texto
-                  fontSize: 28),//tamanho letra texto
-                  textAlign: TextAlign.center,// para otexto ficar bem no centro 
-                ),
-              ),
-              ElevatedButton(
-                onPressed: (){
-                  /*Criei uma função anonima*/
-                  print('Você apertou o Botão');
-                }, 
-                
-                child: Text('Aperte o botão'),),
-
-            ],
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Tarefas'),
           ),
+          body: Container(
+            color: Colors.pink,
+            child: Stack(
+              children: [
+                Container(
+                  color: Colors.blue,
+                  height: 140,
+                ),
+                Container(
+                  color: Colors.white30,
+                  height: 100,
+                )
+              ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(onPressed: () {}),
         ));
   }
 }
---
+++ lib/minha_primeira_tela.dart
@@ -0,0 +1,99 @@
+/*Container(
+          color: const Color.fromARGB(255, 91, 151, 199),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Stack(
+                alignment: AlignmentDirectional.centerStart,
+                children: [
+                  Container(
+                    color: Colors.pink,
+                    width: 500,
+                    height: 100,
+                  ),
+                  Container(
+                    color: Colors.blue,
+                    width: 50,
+                    height: 50,
+                  ),
+                ],
+              ),
+              Container(
+                color: Colors.white,
+                width: 500,
+                height: 100,
+                child: Text(
+                    'aaaaaaa',
+                   
+                ),
+              ),
+              Stack(
+                alignment: AlignmentDirectional.centerStart,
+                children: [
+                  Container(
+                    color: Colors.pink,
+                    width: 500,
+                    height: 100,
+                  ),
+                  Container(
+                    color: Colors.blue,
+                    width: 50,
+                    height: 50,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    color: Colors.black,
+                    width: 50,
+                    height: 50,
+                  ),
+                  Container(
+                    color: Colors.pink,
+                    width: 50,
+                    height: 50,
+                  ),
+                  Container(
+                    color: Colors.black,
+                    width: 50,
+                    height: 50,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(color: Colors.black,width: 60,height: 60,),
+                  Container(color: Colors.pink, width: 200, height: 60,),
+                  Container(color: Colors.black, width: 60,height: 60,),
+                ],
+              ),
+              Container(//texto
+                color: Color.fromARGB(255, 219, 150, 174),
+                width: 300,
+                height: 30,
+                //Inserir texto crio um filho widget chamado text
+                child: Text(
+                  'Diamante amarelo',
+                  style: TextStyle(//estilo do texto 
+                  color: Colors.black,//cor texto
+                  fontSize: 28),//tamanho letra texto
+                  textAlign: TextAlign.center,// para otexto ficar bem no centro 
+                ),
+              ),
+              ElevatedButton(
+                onPressed: (){
+                  /*Criei uma função anonima*/
+                  print('Você apertou o Botão');
+                }, 
+                
+                child: Text('Aperte o botão'),),
+
+            ],
+          ),
+        ), */
