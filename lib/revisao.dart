5e59970fc922cfa361894f7acfc638ca584c320d
DieleCorrea <dybs1520@gmail.com>
2023-09-13 10:37:47 -0300

    centralizei a stack

--- lib/revisao.dart
+++ lib/revisao.dart
@@ -1,50 +1,46 @@
 import 'package:flutter/material.dart';
 
- class revisao extends StatelessWidget {
+class revisao extends StatelessWidget {
   const revisao({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'revisão do que aprendi',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
+        title: 'revisão do que aprendi',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
               seedColor: Color.fromARGB(255, 158, 102, 134)),
           useMaterial3: true,
-      ),
-      home: Scaffold(//estrutura do meu app 
-        appBar: AppBar
-        (title: Text('Testandp App'),
-         ),
-         body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          
-           children: [
-             Container(
-              color: Colors.black38,
-              child: Container(
+        ),
+        home: Scaffold(
+          //estrutura do meu app
+          appBar: AppBar(
+            title: Text('Testandp App'),
+          ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
                 color: Color.fromARGB(255, 134, 63, 156),
-                height: 300,
+                height: 600,
                 width: 500,
+                //child: Text(),
+
                 child: Stack(
+                  alignment: Alignment.center,
                   children: [
                     Container(
                       alignment: Alignment.topCenter,
-                     color: const Color.fromARGB(255, 198, 109, 139),
-                     height: 200,width: 300,
-                     
-                     
-                    
-                    )
-                    
+                      color: const Color.fromARGB(255, 198, 109, 139),
+                      height: 200,
+                      width: 300,
+                    ),
                   ],
                 ),
               ),
-             ),
-           ],
-         ), 
-      )
-    );
+            ],
+          ),
+        ));
   }
 }
-
