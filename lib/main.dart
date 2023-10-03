7f8d5d67532df1abfdfe91d4b228422fee478f96
DieleCorrea <dybs1520@gmail.com>
2023-10-03 14:48:52 -0300

    AnimatedOpacity

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:segundoapp/revisao.dart';
-
-//void main() {
- // runApp(const MyApp());
-//}
+//import 'package:segundoapp/revisao.dart';
 
 void main() {
-  runApp(revisao());
- }
+  runApp(const MyApp());
+}
+
+////void main() {
+ // runApp(revisao());
+// }
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -25,35 +25,39 @@ class MyApp extends StatelessWidget {
             leading: Container(),
             title: Text('Tarefas'),
           ),
-          body: ListView(
-            children: [
-              Task(
-                  'Estudar hoje a noite no liberato aquela aula chata de gestão',
-                  'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
-                  2),
-              Task(
-                  'Academia',
-                  'https://tswbike.com/wp-content/uploads/2020/09/108034687_626160478000800_2490880540739582681_n-e1600200953343.jpg',
-                  3),
-              Task(
-                  'Janta',
-                  'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg',
-                  2),
-              Task('Ingles',
-                  'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg', 4),
-              Task(
-                  'Consulta',
-                  'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
-                  1),
-              Task(
-                  'Arrumar roupas',
-                  'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
-                  5),
-              Task(
-                  'Remedio',
-                  'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
-                  3),
-            ],
+          body: AnimatedOpacity(
+            opacity: 0,
+            duration: Duration(milliseconds: 800),
+            child: ListView(
+              children: [
+                Task(
+                    'Estudar hoje a noite no liberato aquela aula chata de gestão',
+                    'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+                    2),
+                Task(
+                    'Academia',
+                    'https://tswbike.com/wp-content/uploads/2020/09/108034687_626160478000800_2490880540739582681_n-e1600200953343.jpg',
+                    3),
+                Task(
+                    'Janta',
+                    'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg',
+                    2),
+                Task('Ingles',
+                    'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg', 4),
+                Task(
+                    'Consulta',
+                    'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+                    1),
+                Task(
+                    'Arrumar roupas',
+                    'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+                    5),
+                Task(
+                    'Remedio',
+                    'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+                    3),
+              ],
+            ),
           ),
           floatingActionButton: FloatingActionButton(onPressed: () {}),
         ));
